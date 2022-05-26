63ffb5e519699d1ba5dd13112c81ec0e392f497a
cai30304 <cai30304@gmail.com>
2022-05-26 18:00:59 +0800

    [modify] dashboard / nav

--- resources/views/livewire/sidebar-menu.blade.php
+++ resources/views/livewire/sidebar-menu.blade.php
@@ -7,10 +7,38 @@
             <ul
                 class="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
                 <li class="mr-3 flex-1">
+                    <a href="/dashboard"
+                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Dashboard</span>
+                    </a>
+                </li>
+                <li class="mr-3 flex-1">
+                    <a href="/dashboard"
+                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Create RFQ</span>
+                    </a>
+                </li>
+                <li class="mr-3 flex-1">
                     <a href="#"
                         class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
                         <i class="fas fa-tasks pr-0 md:pr-3"></i><span
-                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Products</span>
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Find Products</span>
+                    </a>
+                </li>
+                <li class="mr-3 flex-1">
+                    <a href="#"
+                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Current Case</span>
+                    </a>
+                </li>
+                <li class="mr-3 flex-1">
+                    <a href="#"
+                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Finish Case List</span>
                     </a>
                 </li>
                 {{-- <li class="mr-3 flex-1">
