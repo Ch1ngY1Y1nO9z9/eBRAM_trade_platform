072aeffd405a417c35f56754ffe417b7a0dbdb8c
Ch1ngY1Y1O9z9 <l123050891@gmail.com>
2022-06-16 18:50:14 +0800

    Update 2022_06_16_104735_change_password_to_users_table.php

--- database/migrations/2022_06_16_104735_change_password_to_users_table.php
+++ database/migrations/2022_06_16_104735_change_password_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table            $table->string('password');->change();
+            $table->string('password')->nullable()->change();
         });
     }
 
